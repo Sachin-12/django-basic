47a50ec55795dfeb1671609df58adef1aca28fcc
Sachin-12 <sachindraragul@gmail.com>
2019-04-02 02:05:42 +0530

    first commit

---
+++ basic/basic_app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+from basic_app.models import UserProfileInfo
+
+admin.site.register(UserProfileInfo)
+
+
+# SuperUser
+# name = sachin
+# email = sachin@gmail.com
+# pass = Tata@1234
